145a0e0689f2a8e549e114390906d5183826863f
morshedul-antor <antor.morshedul@gmail.com>
2025-01-15 14:00:10 +0600

    feat: users page create

---
+++ pages/users/[path].js
@@ -0,0 +1,46 @@
+import styles from "../../styles/Home.module.css";
+import { Home } from "../../components";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Users({ data }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h2 className={styles.title}>
+          <Link href="/">Users ({(data && data.length) || 0})</Link>
+        </h2>
+
+        <Home />
+
+        <ol>
+          {data &&
+            data.map((item) => (
+              <>
+                <li key={item.id}>{item.name}</li> <br />
+              </>
+            ))}
+        </ol>
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { name, skip, limit } = context.query;
+
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users?q=${name}&_start=${skip}&_limit=${limit}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
